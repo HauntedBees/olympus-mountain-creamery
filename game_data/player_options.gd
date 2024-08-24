79ec06c673440784db2c9b7cc1f3cc0142d11ce5
Haunted Bees <fench@hauntedbees.com>
2024-08-24 13:22:10 -0700

    initial checkin

---
+++ game_data/player_options.gd
@@ -0,0 +1,5 @@
+## Options related to sound, accessibility, etc.
+class_name PlayerOptions extends Resource
+
+## The multiplier applied to various in-game actions.
+@export var speed_multiplier := 1.0
