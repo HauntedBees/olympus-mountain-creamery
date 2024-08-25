c3e3be3f95702a9dcc5821a324d61bf596a4d14b
Haunted Bees <fench@hauntedbees.com>
2024-08-24 17:33:08 -0700

    milkin it

--- game_scenes/01_make_yog/milk_pot_data.gd
+++ game_scenes/01_make_yog/milk_pot_data.gd
@@ -10,4 +10,7 @@ class_name MilkPotData extends RefCounted
 @export_range(0.0, 1.0) var bacteria_amount := 1.0
 
 ## Lower when the temperature is too low when adding bacteria.
-@export_range(0.3, 1.0) var fermentation_time_multiplier := 1.0
+@export_range(0.3, 1.0) var fermentation_time_diminisher := 1.0
+
+## Increased by quality of pours and other factors.
+@export var quality_multiplier := 1.0
