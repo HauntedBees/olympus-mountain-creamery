0ba6f20a836b4fe2294fa4aa36764e4bd14e924a
Haunted Bees <fench@hauntedbees.com>
2024-08-26 08:28:55 -0700

    informational milks

--- game_scenes/00_title/title_scene.gd
+++ game_scenes/00_title/title_scene.gd
@@ -12,6 +12,7 @@ var _step := State.Main
 @onready var _options: Array[ActionPrompt] = [%Toggle, %Speed, %Timer, %MultiPour, %RemapPrimary, %RemapSecondary, %Return]
 @onready var _remap_dialog: Control = %Remap
 @onready var _remap_text: Label = %RemapText
+@onready var _option_info: Label = %OptionInfo
 
 func _process(delta: float) -> void:
 	_timer -= delta
@@ -108,3 +109,11 @@ func _update_options() -> void:
 			o.state = ActionPrompt.State.Next
 		else:
 			o.state = ActionPrompt.State.None
+	match _option_idx:
+		0: _option_info.text = "Sometimes you'll need to hold down a Button to perform an action. \"Push to Toggle\" lets you press the Button to start and stop the action instead."
+		1: _option_info.text = "Set the speed for various in-game actions, like how fast milk is poured. Higher speeds are more difficult."
+		2: _option_info.text = "When enabled, you will have to complete the Gods' requests in a certain amount of time or else you'll fail."
+		3: _option_info.text = "By default, you must hold the Primary Button to pour liquids, and you'll stop once you release. \"Multi-Pour\" lets you start & stop pouring multiple times."
+		4: _option_info.text = "Remap the Primary Button, which defaults to the Z key on a QWERTY keyboard and the A button on a standard PC gamepad."
+		5: _option_info.text = "Remap the Secondary Button, which defaults to the X key on a QWERTY keyboard and the B button on a standard PC gamepad."
+		6: _option_info.text = "Save your changes and return to the title screen so you can get your yogurt journey started!"
